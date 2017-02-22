12905071c93229e6c90002339d64f0a230877100
NeilMcB <s1305440@sms.ed.ac.uk>
2017-02-21 17:27:18 -0800

    Updated to use makefile expressions

--- ass4/ass4.py
+++ ass4/ass4.py
@@ -1,31 +1,36 @@
 import euler
+import sys
+
+filename = sys.argv[1]
 
 #Directory for images
 path = "images/"
+file = ".pdf"
 
-#Q1
-euler.trajectory_plot(0,1,0.01,10000,"Explicit",path+"Sec1Plt1.pdf")
-
-#Q2
-euler.error_plot(0,1,0.01,10000,"Explicit",path+"Sec1Plt2.pdf")
-
-#Q3
-euler.trunc_err(0,1,0.01,1000,"Explicit",path+"Sec1Plt3.pdf")
-
-#Q4
-euler.error_plot(0,1,0.01,10000,"Explicit",path+"Sec1Plt4.pdf",E_plot=True)
-
-#Q5
-euler.error_plot(0,1,0.01,10000,"Implicit",path+"Sec1Plt5.pdf",E_plot=True)
-
-#Q6
-euler.phase_space(0,1,0.01,1000,["Implicit","Explicit"],path+"Sec2Plt1.pdf")
-
-#Q7
-euler.phase_space(0,1,0.2,500,["Symplectic"],path+"Sec2Plt2.pdf")
-
-#Q8
-euler.error_plot(0,1,0.01,10000,"Symplectic",path+"Sec2Plt3.pdf",E_plot=True)
-
-#Q9
-euler.phase_lag(0,1,0.2,500,path+"Sec2Plt4.pdf")
+if  (filename==path+"Sec1Plt1.pdf"):
+	#Q1
+	euler.trajectory_plot(0,1,0.01,10000,"Explicit",filename)
+elif(filename==path+"Sec1Plt2.pdf"):
+	#Q2
+	euler.error_plot(0,1,0.01,10000,"Explicit",filename)
+elif(filename==path+"Sec1Plt3.pdf"):
+	#Q3
+	euler.trunc_err(0,1,0.01,1000,"Explicit",filename)
+elif(filename==path+"Sec1Plt4.pdf"):
+	#Q4
+	euler.error_plot(0,1,0.01,10000,"Explicit",filename,E_plot=True)
+elif(filename==path+"Sec1Plt5.pdf"):
+	#Q5
+	euler.error_plot(0,1,0.01,10000,"Implicit",filename,E_plot=True)
+elif(filename==path+"Sec2Plt1.pdf"):
+	#Q6
+	euler.phase_space(0,1,0.01,1000,["Implicit","Explicit"],filename)
+elif(filename==path+"Sec2Plt2.pdf"):
+	#Q7
+	euler.phase_space(0,1,0.2,500,["Symplectic"],filename)
+elif(filename==path+"Sec2Plt3.pdf"):
+	#Q8
+	euler.error_plot(0,1,0.01,10000,"Symplectic",filename,E_plot=True)
+elif(filename==path+"Sec2Plt4.pdf"):
+	#Q9
+	euler.phase_lag(0,1,0.2,500,filename)
